1c4d03026e7e79b04c1c965654ace63919bfcc74
manava10 <mmanavp10@gmail.com>
2025-07-04 00:41:19 +0530

    Updated

--- src/DP/ZeroOneKnapsack/EqualSumPartitionTab.java
+++ src/DP/ZeroOneKnapsack/EqualSumPartitionTab.java
@@ -3,13 +3,33 @@ class EqualSumPartitionTab{
     public static void main(String[] args) {
         //We are writint the most effficient code for equal sum partitioning .
         //Here, We go baby.
-        int[] arr = {1,5,11,5};
+        int[] arr = {1,5,10,8};
         int total = 0;
+        int n = arr.length;
         for(int num:arr){
             total+=num;
         }
         if(total%2!=0){
             System.out.println("The given array cannot be partioned ");
+        }else{
+            Boolean[][] dp = new Boolean[n+1][(total/2)+1]; // This is a
+            //Base Case
+            for(int j=0;j<=(total/2);j++){
+                dp[0][j] = false;
+            }
+            for(int i=0;i<=n;i++){
+                dp[i][0] = true;
+            }
+            for(int i=1;i<=n;i++){
+                for(int j=1;j<=(total/2);j++){
+                    if(arr[i-1]<=j){
+                        dp[i][j] = dp[i-1][j] || dp[i-1][j-arr[i-1]];
+                    }else{
+                        dp[i][j] = dp[i-1][j];
+                    }
+                }
+            }
+            System.out.println("The given array can be equally partitioned :"+dp[n][total/2]);
         }
     }
 }
---
+++ src/DP/ZeroOneKnapsack/MinimumSubsetSumDiffTab.java
@@ -0,0 +1,15 @@
+package DP.ZeroOneKnapsack;
+class MinimumSubsetSumDiffTab{
+    public static void main(String[] args) {
+        //
+        int[] arr = {1, 6, 11, 5}; //Have to apply dynamic programming  to find the min.
+        int totalSum = 0;
+        for(int num:arr){
+            totalSum += num;
+        }
+
+
+
+    }
+
+}
