ebd16f660d71f6a0c5ca9f6fa4d827331669dace
manava10 <mmanavp10@gmail.com>
2025-07-06 15:34:02 +0530

    String

--- src/DP/String/lcsTabulation.java
+++ src/DP/String/lcsTabulation.java
@@ -3,10 +3,22 @@ class lcsTabulation{
     public static void main(String[] args) {
         String a = "abcde";
         String b = "bcde";
-        int i = a.length();
-        int j = b.length();
-        int[][] dp =new int[i][j];
-
+        System.out.println("The length of lcs in the given substring is :"+lcsLength(a,b));
+    }
+    public static int lcsLength(String a,String b){
+        //First of all we have to make a dp Table.
+        int[][] dp = new int[a.length()+1][b.length()+1];
+        //There is no need of initialization dont worry.
+        for(int i=1;i<=a.length();i++){
+            for(int j=1;j<=b.length();j++){
+                if(a.charAt(i-1)==b.charAt(j-1)){
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.max(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[a.length()][b.length()];
 
     }
 }
